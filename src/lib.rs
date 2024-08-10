4adddb448fea509451f8bf8faeaf66f11b864e10
Bennett G Sewell <bennett.sewell@bevdevs.com>
2024-08-10 13:30:03 -0400

    Added code that utilizes crate to see that if that triggers dependabot.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+pub fn hello_world() {
+    println!("{:?}", rust_iso3166::ALL_ALPHA2);
+}
+
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -10,5 +15,10 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_hello_world() {
+        hello_world();
     }
 }
